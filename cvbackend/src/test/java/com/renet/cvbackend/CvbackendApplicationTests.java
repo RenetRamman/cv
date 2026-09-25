9c4c2af2bd6391a71fb8563565dced95ec2a3bd3
RenetRamman <renet.ramman@gmail.com>
2026-09-25 13:36:22 +0300

    Added docker-compose for the database and configured initial database connection

--- cvbackend/src/test/java/com/renet/cvbackend/CvbackendApplicationTests.java
+++ cvbackend/src/test/java/com/renet/cvbackend/CvbackendApplicationTests.java
@@ -1,13 +1,29 @@
 package com.renet.cvbackend;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CvbackendApplicationTests {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void canConnectToPostgreSQL() throws Exception {
+		try (Connection connection = dataSource.getConnection()) {
+			assertThat(connection.isValid(5)).isTrue();
+			assertThat(connection.getMetaData().getDatabaseProductName()).isEqualTo("PostgreSQL");
+		}
 	}
 
 }
